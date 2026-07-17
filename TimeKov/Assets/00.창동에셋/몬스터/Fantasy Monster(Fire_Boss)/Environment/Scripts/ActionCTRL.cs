8a6af5f12b89cbc53320000428448e62de7acf31
gkswhddnr <gkswhddnr7593@naver.com>
2026-07-17 13:21:06 +0900

    몬스터 에셋 세팅

---
+++ TimeKov/Assets/00.창동에셋/몬스터/Fantasy Monster(Fire_Boss)/Environment/Scripts/ActionCTRL.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionCTRL : MonoBehaviour {
+
+
+	public float rotateSpeed = 1.0f;
+
+	[System.Serializable]
+	public class actControlParam
+	{
+		public KeyCode key = new KeyCode();
+		public string paramName;
+	}
+	public actControlParam[] actionPlayKeys;
+
+
+	[System.Serializable]
+	public class eyesControlParam
+	{
+		public KeyCode key = new KeyCode();
+		public string paramName;
+	}
+	public eyesControlParam[] eyesToggleKeys;
+
+
+	[System.Serializable]
+	public class mouseControlParam
+	{
+		public KeyCode key = new KeyCode();
+		public string paramName;
+	}
+	public mouseControlParam[] mouseToggleKeys;
+
+
+	private Animator curAnimator;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		if ( null != this.gameObject.GetComponent<Animator>() )
+		{
+			curAnimator = this.gameObject.GetComponent<Animator> ();;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( curAnimator != null )
+		{	
+			for (int i = 0; i < actionPlayKeys.Length; i++) 
+			{
+				if ( Input.GetKeyDown( actionPlayKeys[i].key ) )
+				{
+					curAnimator.SetBool (actionPlayKeys [i].paramName, true);
+				}
+
+				if (Input.GetKeyUp(actionPlayKeys [i].key)) 
+				{
+					curAnimator.SetBool (actionPlayKeys [i].paramName, false);
+				}
+			}
+
+			for (int i = 0; i < eyesToggleKeys.Length; i++) 
+			{
+				if ( Input.GetKeyDown( eyesToggleKeys[i].key ) )
+				{
+					if (curAnimator.GetBool (eyesToggleKeys [i].paramName) == false) 
+					{
+						curAnimator.SetBool (eyesToggleKeys [i].paramName, true);
+					}
+					else if (curAnimator.GetBool (eyesToggleKeys [i].paramName) == true) 
+					{
+						curAnimator.SetBool (eyesToggleKeys [i].paramName, false);
+					}
+
+					for (int j = 0; j < eyesToggleKeys.Length; j++) 
+					{
+						if (j != i) 
+						{
+							curAnimator.SetBool( eyesToggleKeys[j].paramName, false);
+						}
+					}
+				}
+			}
+
+			for (int i = 0; i < mouseToggleKeys.Length; i++) 
+			{
+				if ( Input.GetKeyDown( mouseToggleKeys[i].key ) )
+				{
+					if (curAnimator.GetBool (mouseToggleKeys [i].paramName) == false) 
+					{
+						curAnimator.SetBool (mouseToggleKeys [i].paramName, true);
+					}
+					else if (curAnimator.GetBool (mouseToggleKeys [i].paramName) == true) 
+					{
+						curAnimator.SetBool (mouseToggleKeys [i].paramName, false);
+					}
+
+					for (int j = 0; j < mouseToggleKeys.Length; j++) 
+					{
+						if (j != i) 
+						{
+							curAnimator.SetBool( mouseToggleKeys[j].paramName, false);
+						}
+					}
+				}
+			}
+		}
+
+		if ( Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate( 0, Time.deltaTime * (rotateSpeed), 0);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(0, Time.deltaTime * (-rotateSpeed), 0);
+		}
+	}
+
+	bool CheckAniClip ( string clipname )
+	{	
+		if (this.GetComponent<Animation> ().GetClip (clipname) == null) 
+		{
+			return false;
+		} 
+		else if (this.GetComponent<Animation> ().GetClip (clipname) != null) 
+		{
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ TimeKov/Assets/00.창동에셋/몬스터/Fantasy Monster(Fire_Boss)/Environment/Scripts/uv_move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uv_move : MonoBehaviour {
+
+
+    public Vector2[] uvSpeedSet;
+	//public Vector2 uvSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < uvSpeedSet.Length; i++)
+        {
+            //Vector2 newUVset = this.gameObject.GetComponent<Renderer>().material.mainTextureOffset + (uvSpeed * Time.deltaTime);
+            //newUVset = this.gameObject.GetComponent<Renderer>().materials[i].GetTextureOffset("_BumpMap") + (uvSpeedSet[i] * Time.deltaTime);
+            //this.gameObject.GetComponent<Renderer>().materials[i].SetTextureOffset("_BumpMap", newUVset);
+
+            Vector2 newUV = this.gameObject.GetComponent<Renderer>().materials[i].mainTextureOffset + (uvSpeedSet[i] * Time.deltaTime);
+
+            this.gameObject.GetComponent<Renderer>().materials[i].mainTextureOffset = newUV;
+        }
+
+		//Vector2 newUV = this.gameObject.GetComponent<Renderer> ().material.mainTextureOffset + (uvSpeed * Time.deltaTime);
+
+		//this.gameObject.GetComponent<Renderer> ().material.mainTextureOffset = newUV;
+
+	}
+}
